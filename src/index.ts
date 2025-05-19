fe1a4568dbcdff0916d9bfacc360632e0b7d9c30
omarellahi <omarellahi1@gmail.com>
2025-05-19 17:23:05 +0500

    fixes

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import AI from "./tools/AI.js";
 import Listener from "./tools/Listener.js";
 import Speaker from "./tools/Speaker.js";
 
-const li = new Listener(6000, 'vosk-model-en-us-0.22', 'hello');
+const li = new Listener(6000, 'vosk-model-en-us-0.42-gigaspeech', 'hello');
 const ai = new AI('llama3.2');
 const sp = new Speaker();
 
