15bc13d42f7f6ecf700d75bf03631216f70e24b1
omarellahi <omarellahi1@gmail.com>
2025-05-19 14:58:34 +0500

    fixes

--- src/tools/Listener.ts
+++ src/tools/Listener.ts
@@ -1,6 +1,6 @@
-import Mic from 'mic';
-import Vosk from 'vosk';
+import Mic from 'node-mic';
 import path from 'path';
+import { Model, Recognizer } from 'vosk-koffi';
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -8,20 +8,21 @@ import { dirname } from 'path';
 export default class Listener {
   private micInstance: any;
   private micInputStream: any;
-  private model: Vosk.Model;
-  private recognizer: Vosk.Recognizer<any>;
+  private model: Model;
+  private recognizer: Recognizer<any>;
   public activationWord: string;
 
   private audioTimeout: number;
   public isListeningActive: boolean;
 
   constructor(audioTimeout: number, voskModel: string, activationWord: string) {
-    this.micInstance = Mic({
-      rate: '16000',
-      channels: '1',
-      exitOnSilence: 5,
-      device: 'plughw:0,0',
-    })
+    this.micInstance = new Mic({
+      rate: 16000,
+      channels: 1,
+      threshold: 6
+    });
+
+  
     this.micInputStream = this.micInstance.getAudioStream();
     this.micInputStream.on('end', () => {
       console.log('Microphone stream ended.');
@@ -30,9 +31,9 @@ export default class Listener {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = dirname(__filename);
     const modelPath = path.join(__dirname, '../../models', voskModel);
-    this.model = new Vosk.Model(modelPath);
+    this.model = new Model(modelPath);
 
-    this.recognizer = new Vosk.Recognizer({ model: this.model, sampleRate: 16000 });
+    this.recognizer = new Recognizer({ model: this.model, sampleRate: 16000 });
     this.activationWord = activationWord;
 
     this.audioTimeout = audioTimeout;
